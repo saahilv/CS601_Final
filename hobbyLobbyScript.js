1c510407f3b4fc4aa373ba00b4d4292fc14942bd
saahilv <saahilv@bu.edu>
2022-12-11 00:44:28 +0530

    Add hobby gallary

--- hobbyLobbyScript.js
+++ hobbyLobbyScript.js
@@ -1,34 +1,18 @@
 const {createApp} = Vue
 
-const sourceImg = {
-    0:  { 
-        src: "Media/Piano.png",
-        alt: "Playing the Piano"
-    },
-    1:  {
-        src: "Media/Guitar.png",
-        alt: "Playing the Guitar"
-    },
-    2:  {
-        src: "Media/Paddleboard.png",
-        alt: "Paddleboarding"
-    },
-    3:  {
-        src: "Media/Travel.png",
-        alt: "Traveling"
-    }
-}
-
 createApp({
     data() {
         return {
-            count: 0,
-            imageSource: sourceImg[0],
+            showMessage: false,
+            message: ""
         }
     }, methods: {
-        changeImage() {
-            this.count = (this.count + 1) % 4;
-            this.imageSource = sourceImg[this.count];
-        } 
+        showMessage(message) {
+            this.showMessage = true;
+            this.message = message;
+        },
+        hideMessage() {
+            this.showMessage = false;
+        }
     }
 }).mount('#app')
