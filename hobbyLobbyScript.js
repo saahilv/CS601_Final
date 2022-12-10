ea08534847a323a57da2307bd9506890c4d8bc26
saahilv <saahilv@bu.edu>
2022-12-11 01:05:34 +0530

    Add message

--- hobbyLobbyScript.js
+++ hobbyLobbyScript.js
@@ -1,5 +1,14 @@
 const {createApp} = Vue
 
+const overlayMessages = {
+    "guitar": "I have been playing the guitar since 20xx and was in my high school rock band.",
+    "piano": "I have been playing the piano.",
+    "travel": "I love to travel.",
+    "netflix": "My favorite way to relax is by watching TV",
+    "gym": "I go to the gym to feel good and sometimes my wife manages to drag me out on a run",
+    "paddleboard": "I love to paddleboard after work during the summer",
+}
+
 createApp({
     data() {
         return {
@@ -7,9 +16,9 @@ createApp({
             message: ""
         }
     }, methods: {
-        changeMessage(message) {
+        changeMessage(image) {
             this.showMessage = true;
-            this.message = message;
+            this.message = overlayMessages[image];
         },
         hideMessage() {
             this.showMessage = false;
