9967d152f4684abd75f80cb73d419905158f52d4
saahilv <saahilv@bu.edu>
2022-12-11 10:37:14 +0530

    Replace images

--- hobbyLobbyScript.js
+++ hobbyLobbyScript.js
@@ -1,12 +1,21 @@
 const {createApp} = Vue
 
 const overlayMessages = {
+    "amsterdam": "TODO",
+    "cliff": "TODO",
     "guitar": "I have been playing the guitar since 20xx and was in my high school rock band.",
+    "italy": "TODO",
+    "keys": "TODO",
+    "luau": "TODO",
+    "mailbox": "TODO",
+    "maple": "TODO",
+    "paddleboarding": "TODO",
     "piano": "I have been playing the piano.",
-    "travel": "I love to travel.",
-    "netflix": "My favorite way to relax is by watching TV",
-    "gym": "I go to the gym to feel good and sometimes my wife manages to drag me out on a run",
-    "paddleboard": "I love to paddleboard after work during the summer",
+    "rainier": "TODO",
+    "rainier2": "TODO",
+    "rattlesnake": "TODO",
+    "workingout": "I go to the gym to feel good and sometimes my wife manages to drag me out on a run",
+    "yellowstone": "TODO",
 
 }
 
