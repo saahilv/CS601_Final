b29ae2c057abbbf00bc3e660449223968be2f87f
saahilv <saahilv@bu.edu>
2022-12-11 13:04:58 +0530

    Added photo descriptions

--- hobbyLobbyScript.js
+++ hobbyLobbyScript.js
@@ -1,22 +1,22 @@
 const {createApp} = Vue
 
 const overlayMessages = {
-    "amsterdam": "TODO",
-    "cliff": "TODO",
-    "guitar": "I have been playing the guitar since 20xx and was in my high school rock band.",
-    "italy": "TODO",
-    "keys": "TODO",
-    "luau": "TODO",
-    "mailbox": "TODO",
-    "maple": "TODO",
-    "mexico": "TODO",
-    "paddleboarding": "TODO",
-    "piano": "I have been playing the piano.",
-    "rainier": "TODO",
-    "rainier2": "TODO",
-    "rattlesnake": "TODO",
+    "amsterdam": "Hanging out with my younger brother in amsterdam",
+    "cliff": "Helcopter ride to a cliff in the middle of nowhere in Maui",
+    "guitar": "I have been playing the guitar since 2005 and was in my high school rock band.",
+    "italy": "Trying to push the leaning tower of Pisa beyond its limits",
+    "keys": "Playing the keys for a rock concert in New Delhi",
+    "luau": "Enjoying a luau with my wife",
+    "mailbox": "12 miles and 4000 ft hike later, we were completely exhausted",
+    "maple": "Hiking on maple pass",
+    "mexico": "Posing in front of one of the wonders of the world",
+    "paddleboarding": "Paddleboarding on Lake Washington close to sunset",
+    "piano": "Playing the piano for an audience in Lianoning Province, China",
+    "rainier": "Hike to Mt. Rainier",
+    "rainier2": "Another hike to Mt. Rainier",
+    "rattlesnake": "Hiking up rattlesnake ledge",
     "workingout": "I go to the gym to feel good and sometimes my wife manages to drag me out on a run",
-    "yellowstone": "TODO",
+    "yellowstone": "Love visiting national parks, this one is from yellowstone, one of my favorite National Parks in US",
 
 }
 
